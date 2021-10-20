9c19e096d7283d9f567b7aa905f4d97cb2ffdff1
Darya Anishchenko <Darya.Anishchenko@bonava.com>
2021-10-21 01:53:52 +0300

    Добавьте файлы проекта.

---
+++ Task1_Animals/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание и вызов метода класса Кот
+            Cat cat = new Cat();
+            cat.ShowInfo();
+
+            Console.WriteLine("");
+            //Создание и вызов метода класса Пёс
+            Dog dog = new Dog();
+            dog.ShowInfo();
+             
+
+            Console.ReadKey();
+        }
+    }
+
+
+    abstract class Animal
+    {
+        //Абстрактное свойство
+        public abstract string Naming { set; get; }
+        //Конструктор для значения по умолчанию
+        public Animal()
+        {
+            this.Naming = "Животное";
+        }
+        //Абстрактный метод голоса
+        public abstract void Say();
+        //Метод обычный
+        public void ShowInfo()
+        {
+            Console.Write("{0} говорит: ", Naming);
+            Say();
+            Console.Write("!");
+        }           
+    }
+
+    class Cat : Animal
+    {
+        public override string Naming 
+        { 
+            set
+            {
+                value = "Кот";
+            }
+            get
+            {
+                return "Кот";
+            }
+        }
+
+        public override void Say()
+        {
+            Console.Write("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        public override string Naming { get => "Пёс"; set => value = "Пёс"; }
+
+        public override void Say()
+        {
+            Console.Write("Гав");
+        }
+    }
+
+}
+
